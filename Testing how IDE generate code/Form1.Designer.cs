972ad66d32f938d6315911e2e20effac775a9fbc
Hajk <tunczorx@gmail.com>
2018-03-08 11:27:08 +0100

    Testing how IDE genereta code

--- Testing how IDE generate code/Form1.Designer.cs
+++ Testing how IDE generate code/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Testing_how_IDE_generate_code.Properties.Resources.Witcher_3_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(-4, -21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(803, 488);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption; // Whenever i use IDE to change something, my all comments disappear. 
+            this.button1.Location = new System.Drawing.Point(294, 396);         // When i added background colour IDE genereted new line of code. 
+            this.button1.Name = "button1";                                      // I'm able to change text, colour etc editing this part of code.
+            this.button1.Size = new System.Drawing.Size(201, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "I\'m doing nothing :("; //
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
